a76682ebadbb20747cecea01f8b074aaee360178
Oliver Mesieh <olivermesieh@gmail.com>
2025-11-30 14:31:20 +0100

    Add third-party license info to build output

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import viteCompression from 'vite-plugin-compression';
+import license from 'rollup-plugin-license';
 
 export default defineConfig({
   build: {
@@ -7,17 +8,36 @@ export default defineConfig({
     sourcemap: false,
     minify: 'esbuild',
     cssCodeSplit: true,
+    esbuild: {
+      legalComments: 'inline'
+    },
     rollupOptions: {
       output: {
+        banner: `/*!
+ * 39C3 Artwork Generator
+ * Licensed under MIT
+ *
+ * Kario39C3VarWEB font is free for 39C3-related use only
+ * Commercial use requires license from Show me Fonts (https://www.showmefonts.com/)
+ *
+ * Third-party dependencies - see LICENSES.txt for details
+ */`,
         entryFileNames: 'assets/[name].[hash].js',
         chunkFileNames: 'assets/[name].[hash].js',
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
+          if (assetInfo.names?.[0]?.endsWith('.css')) {
             return 'assets/[name].[hash].css';
           }
           return 'assets/[name].[hash].[ext]';
         },
       },
+      plugins: [
+        license({
+          thirdParty: {
+            output: 'dist/LICENSES.txt'
+          }
+        })
+      ],
     },
     chunkSizeWarningLimit: 1000,
   },
